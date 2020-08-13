e8bde0aed1dbba338648a31cf4c514b05e91a2c0
Sunil Shetye <sunilshetye@rocketmail.com>
2020-08-13 15:58:09 +0530

    get all categories and blocks

--- blocks/blocks/xcosblocks/tests.py
+++ blocks/blocks/xcosblocks/tests.py
@@ -4,10 +4,17 @@ from . import models
 
 
 class CategoryTestCase(TestCase):
-    def setUp(self):
-        models.Category.objects.create(name='ANDLOG', sort_order=30)
-
     def test_categories(self):
-        category = models.Category.objects.get(name='ANDLOG')
+        category = models.Category.objects.get(id=1)
         print(category)
-        self.assertEqual(category.name, 'ANDLOG')
+        self.assertEqual(category.name, 'Commonly Used Blocks')
+
+        block = models.Block.objects.get(id=1)
+        print(block)
+        self.assertEqual(block.name, 'LOGICAL_OP')
+
+        for category in models.Category.objects.all().order_by('sort_order'):
+            print(category.id, category.name, category.sort_order)
+            for block in models.Block.objects.filter(categories=category.id).order_by('name'):
+                print(block.name)
+            print()
