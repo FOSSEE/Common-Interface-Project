6592bca014365707913bb6b71a069822390162a1
Sunil Shetye <sunilshetye@rocketmail.com>
2021-07-28 14:49:22 +0530

    code cleanup

--- blocks/blocks/xcosblocks/apps.py
+++ blocks/blocks/xcosblocks/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class XcosblocksConfig(AppConfig):
+class XcosBlocksConfig(AppConfig):
     name = 'blocks.xcosblocks'
--- blocks/blocks/xcosblocks/tests.py
+++ blocks/blocks/xcosblocks/tests.py
@@ -1,38 +1,54 @@
 from django.test import TestCase
 
-from .models import Category, Block
+from .models import Category, Block, BlockPort
+
+
+PRINT_BLOCKS = False
+PRINT_PORTS = False
 
 
 class CategoryTestCase(TestCase):
     def test_categories(self):
         category = Category.objects.get(id=1)
         print(category)
-        self.assertEqual(category.name, 'Commonly Used Blocks')
+        self.assertEqual(str(category), 'Commonly Used Blocks')
         print()
 
-        block = Block.objects.get(id=1)
+        block = Block.objects.get(id=66)
         print(block)
-        self.assertEqual(block.name, 'LOGICAL_OP')
+        self.assertEqual(str(block), 'POWBLK_f')
+        if PRINT_PORTS:
+            blockports = block.blockport_set.all()
+            for blockport in blockports:
+                print(blockport)
+        print()
+
         print()
 
         categories = Category.objects.all().order_by('sort_order')
-        for category in categories:
-            blocks = Block.objects.filter(categories=category) \
+        allblocks = Block.objects.all() \
                 .order_by('name')
-            print(category.id, category.name, len(blocks))
-            for block in blocks:
-                print(block.name)
+        for category in categories:
+            blocks = allblocks.filter(categories=category)
+            print(category.id, category, len(blocks))
+            if PRINT_BLOCKS:
+                for block in blocks:
+                    print(block)
+                print()
+        if not PRINT_BLOCKS:
             print()
 
         namestring = 'A'
-        blocks = Block.objects.filter(name__istartswith=namestring)
-        categories = Category.objects.filter(block__in=blocks).distinct()  \
+        allblocks = Block.objects.filter(name__istartswith=namestring)
+        categories = Category.objects.filter(block__in=allblocks).distinct()  \
             .order_by('sort_order')
         for category in categories:
-            blocks = Block.objects.filter(categories=category,
-                                          name__istartswith=namestring) \
+            blocks = allblocks.filter(categories=category) \
                 .order_by('name')
-            print(category.id, category.name, len(blocks))
-            for block in blocks:
-                print(block.name)
+            print(category.id, category, len(blocks))
+            if PRINT_BLOCKS:
+                for block in blocks:
+                    print(block)
+                print()
+        if not PRINT_BLOCKS:
             print()
