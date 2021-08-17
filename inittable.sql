b1b1198c8e91ef1d6c4489bdf3adc5d902bdfd6a
Sunil Shetye <sunilshetye@rocketmail.com>
2021-08-17 16:54:13 +0530

    adding block_name to model

--- inittable.sql
+++ inittable.sql
@@ -472,7 +472,8 @@ DELETE FROM xcosblocks_blockparameter
 UPDATE xcosblocks_block B
     JOIN xcosblocks_blockprefix BP ON BP.id = B.blockprefix_id
     JOIN xcosblocks_category C ON C.id = B.main_category_id
-    SET B.block_image_path = CONCAT('palettes/', B.name, '.png');
+    SET B.block_name = B.name,
+    B.block_image_path = CONCAT('palettes/', B.name, '.png');
 
 UPDATE xcosblocks_block B
     JOIN tmp_xcosblocks_block TB ON TB.id = B.id
