e9aa5cf49e80f9c059974fe8f39771d0d696a871
Sunil Shetye <sunilshetye@rocketmail.com>
2021-09-28 13:36:25 +0530

    give simulation options
    Closes #1

--- blocks/eda-frontend/src/components/SchematicEditor/SimulationProperties.js
+++ blocks/eda-frontend/src/components/SchematicEditor/SimulationProperties.js
@@ -4,6 +4,8 @@ import {
   ListItem,
   Button,
   TextField,
+  InputAdornment,
+  MenuItem,
   ExpansionPanel,
   ExpansionPanelSummary,
   ExpansionPanelDetails,
@@ -45,8 +47,14 @@ export default function SimulationProperties () {
   const dispatch = useDispatch()
   const classes = useStyles()
   const [transientAnalysisControlLine, setTransientAnalysisControlLine] = useState({
-    stop: '30',
-    step: '0.1'
+    final_integration_time: '30',
+    real_time_scaling: '0',
+    integrator_absolute_tolerance: '1E-6',
+    integrator_relative_tolerance: '1E-6',
+    tolerance_on_time: '1E-10',
+    max_integration_time_interval: '100001',
+    solver_kind: '1',
+    maximum_step_size: '0'
   })
 
   const handleTransientAnalysisControlLine = (evt) => {
@@ -69,11 +77,11 @@ export default function SimulationProperties () {
 
   // Prepare Netlist to file
   const prepareNetlist = (netlist) => {
-    var titleA = netfile.title.split(' ')[1]
-    var myblob = new Blob([netlist], {
+    const titleA = netfile.title.split(' ')[1]
+    const myblob = new Blob([netlist], {
       type: 'text/plain'
     })
-    var file = new File([myblob], `${titleA}.xml`, { type: 'text/xml', lastModified: Date.now() })
+    const file = new File([myblob], `${titleA}.xml`, { type: 'text/xml', lastModified: Date.now() })
     // console.log(file)
     sendNetlist(file)
   }
@@ -115,25 +123,25 @@ export default function SimulationProperties () {
         if (res.data.state === 'PROGRESS' || res.data.state === 'PENDING') {
           setTimeout(simulationResult(url), 1000)
         } else {
-          var result = res.data.details
+          const result = res.data.details
           if (result === null) {
             setIsResult(false)
           } else {
             setIsResult(true)
-            var temp = res.data.details.data
-            var data = result.data
+            const temp = res.data.details.data
+            const data = result.data
             // console.log('DATA SIm', data)
             if (res.data.details.graph === 'true') {
-              var simResultGraph = { labels: [], x_points: [], y_points: [] }
+              const simResultGraph = { labels: [], x_points: [], y_points: [] }
               // populate the labels
-              for (var i = 0; i < data.length; i++) {
+              for (let i = 0; i < data.length; i++) {
                 simResultGraph.labels[0] = data[i].labels[0]
-                var lab = data[i].labels
+                const lab = data[i].labels
                 // lab is an array containeing labels names ['time','abc','def']
                 simResultGraph.x_points = data[0].x
 
                 // labels
-                for (var x = 1; x < lab.length; x++) {
+                for (let x = 1; x < lab.length; x++) {
                   if (lab[x].includes('#branch')) {
                     lab[x] = `I (${lab[x].replace('#branch', '')})`
                   }
@@ -145,7 +153,7 @@ export default function SimulationProperties () {
                   simResultGraph.labels.push(lab[x])
                 }
                 // populate y_points
-                for (var z = 0; z < data[i].y.length; z++) {
+                for (let z = 0; z < data[i].y.length; z++) {
                   simResultGraph.y_points.push(data[i].y[z])
                 }
               }
@@ -158,7 +166,7 @@ export default function SimulationProperties () {
 
               dispatch(setResultGraph(simResultGraph))
             } else {
-              var simResultText = []
+              const simResultText = []
               for (let i = 0; i < temp.length; i++) {
                 let postfixUnit = ''
                 if (temp[i][0].includes('#branch')) {
@@ -184,7 +192,7 @@ export default function SimulationProperties () {
   }
 
   const startSimulate = (type) => {
-    var compNetlist = Save()
+    const compNetlist = Save()
     switch (type) {
       case 'Transient':
         dispatch(setResultTitle('Transient Analysis Output'))
@@ -193,7 +201,7 @@ export default function SimulationProperties () {
         break
     }
 
-    var netlist = compNetlist
+    const netlist = compNetlist
 
     prepareNetlist(netlist)
   }
@@ -212,31 +220,88 @@ export default function SimulationProperties () {
             <ExpansionPanel>
               <ExpansionPanelSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
                 <Typography className={classes.heading}>Transient Analysis</Typography>
               </ExpansionPanelSummary>
               <ExpansionPanelDetails>
-                <form className={classes.propertiesBox} noValidate autoComplete="off">
+                <form className={classes.propertiesBox} noValidate autoComplete='off'>
                   <List>
                     <ListItem>
-                      <TextField id="stop" label="Stop Time" size='small' variant="outlined"
-                        value={transientAnalysisControlLine.stop}
+                      <TextField
+                        id='final_integration_time' label='Final integration time' size='small' variant='outlined'
+                        InputProps={{ endAdornment: <InputAdornment position='end'>S</InputAdornment> }}
+                        value={transientAnalysisControlLine.final_integration_time}
                         onChange={handleTransientAnalysisControlLine}
                       />
-                      <span style={{ marginLeft: '10px' }}>S</span>
                     </ListItem>
                     <ListItem>
-                      <TextField id="step" label="Time Step" size='small' variant="outlined"
-                        value={transientAnalysisControlLine.step}
+                      <TextField
+                        id='real_time_scaling' label='Real time scaling' size='small' variant='outlined'
+                        value={transientAnalysisControlLine.real_time_scaling}
                         onChange={handleTransientAnalysisControlLine}
                       />
-                      <span style={{ marginLeft: '10px' }}>S</span>
+                    </ListItem>
+                    <ListItem>
+                      <TextField
+                        id='integrator_absolute_tolerance' label='Integrator absolute tolerance' size='small' variant='outlined'
+                        value={transientAnalysisControlLine.integrator_absolute_tolerance}
+                        onChange={handleTransientAnalysisControlLine}
+                      />
+                    </ListItem>
+                    <ListItem>
+                      <TextField
+                        id='integrator_relative_tolerance' label='Integrator relative tolerance' size='small' variant='outlined'
+                        value={transientAnalysisControlLine.integrator_relative_tolerance}
+                        onChange={handleTransientAnalysisControlLine}
+                      />
+                    </ListItem>
+                    <ListItem>
+                      <TextField
+                        id='tolerance_on_time' label='Tolerance on time' size='small' variant='outlined'
+                        InputProps={{ endAdornment: <InputAdornment position='end'>S</InputAdornment> }}
+                        value={transientAnalysisControlLine.tolerance_on_time}
+                        onChange={handleTransientAnalysisControlLine}
+                      />
+                    </ListItem>
+                    <ListItem>
+                      <TextField
+                        id='max_integration_time_interval' label='Max integration time interval' size='small' variant='outlined'
+                        InputProps={{ endAdornment: <InputAdornment position='end'>S</InputAdornment> }}
+                        value={transientAnalysisControlLine.max_integration_time_interval}
+                        onChange={handleTransientAnalysisControlLine}
+                      />
+                    </ListItem>
+                    <ListItem>
+                      <TextField
+                        id='solver_kind' select label='Solver kind' size='small' variant='outlined'
+                        value={transientAnalysisControlLine.solver_kind}
+                        onChange={handleTransientAnalysisControlLine}
+                      >
+                        <MenuItem value='0'>LSodar</MenuItem>
+                        <MenuItem value='1'>Sundials/CVODE - BDF - NEWTON</MenuItem>
+                        <MenuItem value='2'>Sundials/CVODE - BDF - FUNCTIONAL</MenuItem>
+                        <MenuItem value='3'>Sundials/CVODE - ADAMS - NEWTON</MenuItem>
+                        <MenuItem value='4'>Sundials/CVODE - ADAMS - FUNCTIONAL</MenuItem>
+                        <MenuItem value='5'>DOPRI5 - Dormand-Prince 4(5)</MenuItem>
+                        <MenuItem value='6'>RK45 - Runge-Kutta 4(5)</MenuItem>
+                        <MenuItem value='7'>Implicit RK45 - Implicit Runge-Kutta 4(5) - FIXED-POINT</MenuItem>
+                        <MenuItem value='8'>CRANI - Crank-Nicolson 2(3) - FIXED-POINT</MenuItem>
+                        <MenuItem value='100'>Sundials/IDA</MenuItem>
+                        <MenuItem value='101'>DDaskr - BDF</MenuItem>
+                      </TextField>
+                    </ListItem>
+                    <ListItem>
+                      <TextField
+                        id='maximum_step_size' label='Maximum step size' size='small' variant='outlined'
+                        value={transientAnalysisControlLine.maximum_step_size}
+                        onChange={handleTransientAnalysisControlLine}
+                      />
                     </ListItem>
 
                     <ListItem>
-                      <Button id="transientAnalysisSimulate" size='small' variant="contained" color="primary" onClick={(e) => { startSimulate('Transient') }}>
+                      <Button id='transientAnalysisSimulate' size='small' variant='contained' color='primary' onClick={(e) => { startSimulate('Transient') }}>
                         Simulate
                       </Button>
                     </ListItem>
@@ -246,8 +311,8 @@ export default function SimulationProperties () {
             </ExpansionPanel>
           </ListItem>
 
-          <ListItem style={isSimRes ? {} : { display: 'none' }} onClick={handlesimulateOpen} >
-            <Button size='small' variant="contained" color="primary" style={{ margin: '10px auto' }} onClick={handlesimulateOpen}>
+          <ListItem style={isSimRes ? {} : { display: 'none' }} onClick={handlesimulateOpen}>
+            <Button size='small' variant='contained' color='primary' style={{ margin: '10px auto' }} onClick={handlesimulateOpen}>
               Simulation Result
             </Button>
           </ListItem>
