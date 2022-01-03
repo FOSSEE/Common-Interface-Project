3e16d7742ed1d4b37d5e82bb8424f996daff7a05
Sunil Shetye <sunilshetye@rocketmail.com>
2022-01-04 00:34:47 +0530

    do not add datapoint to state

--- blocks/eda-frontend/src/redux/reducers/index.js
+++ blocks/eda-frontend/src/redux/reducers/index.js
@@ -3,7 +3,6 @@ import schematicEditorReducer from './schematicEditorReducer'
 import componentPropertiesReducer from './componentPropertiesReducer'
 import netlistReducer from './netlistReducer'
 import simulationReducer from './simulationReducer'
-import datapointReducer from './datapointReducer'
 import authReducer from './authReducer'
 import saveSchematicReducer from './saveSchematicReducer'
 import dashboardReducer from './dashboardReducer'
@@ -12,7 +11,6 @@ export default combineReducers({
   componentPropertiesReducer,
   netlistReducer,
   simulationReducer,
-  datapointReducer,
   authReducer,
   saveSchematicReducer,
   dashboardReducer
