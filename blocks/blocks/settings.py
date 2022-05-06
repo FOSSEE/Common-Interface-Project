a2c157614655e3ad7c59e7124f9c8d868b7c71a2
Sunil Shetye <sunilshetye@rocketmail.com>
2022-05-06 16:57:46 +0530

    adding number to broker url

--- blocks/blocks/settings.py
+++ blocks/blocks/settings.py
@@ -149,8 +149,8 @@ MEDIA_URL = '/_files/'
 MEDIA_ROOT = os.path.join('/tmp', 'blocks-tmp')
 
 # celery
-CELERY_BROKER_URL = 'redis://redis:6379'
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
