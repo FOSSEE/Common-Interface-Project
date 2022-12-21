6ef9051b01cdb5902b5843584f39692bb58831b1
Sunil Shetye <sunilshetye@rocketmail.com>
2022-12-21 18:13:53 +0530

    cleaning up code

--- Xcos/blocks/BIGSOM_f.py
+++ Xcos/blocks/BIGSOM_f.py
@@ -7,31 +7,7 @@ def BIGSOM_f(outroot, attribid, ordering, geometry, parameters):
         value='+')
 
     node = addDataNode(outnode, 'ScilabString', **{'as': 'exprs'}, height=1, width=1)
-    addDataData(node, '[1;-1]')
-
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'realParameters'}, height=2, width=1)
-    addDataData(node, 1.0)
-    addDataData(node, -1.0)
-
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'integerParameters'}, height=0, width=0)
-
-    node = addNode(outnode, 'Array', **{'as': 'objectsParameters'},
-        scilabClass='ScilabList')
-
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'nbZerosCrossing'}, height=1, width=1)
-    addDataData(node, 0.0)
-
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'nmode'}, height=1, width=1)
-    addDataData(node, 0.0)
-
-    node = addNode(outnode, 'Array', **{'as': 'oDState'},
-        scilabClass='ScilabList')
-
-    node = addNode(outnode, 'Array', **{'as': 'equations'},
-        scilabClass='ScilabList')
-
-    node = addNode(outnode, 'mxGeometry', **{'as': 'geometry'},
-        height=geometry['height'], width=geometry['width'], x=geometry['x'], y=geometry['y'])
+    addDataData(node, parameters[0])
 
     return outnode
 
--- Xcos/blocks/CSCOPE.py
+++ Xcos/blocks/CSCOPE.py
@@ -17,47 +17,6 @@ def CSCOPE(outroot, attribid, ordering, geometry, parameters):
     addDataData(node, '0')
     addDataData(node, '')
 
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'realParameters'}, height=4, width=1)
-    addDataData(node, 0.0)
-    addDataData(node, parameters[4], True)
-    addDataData(node, parameters[5], True)
-    addDataData(node, 30.0)
-
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'integerParameters'}, height=15, width=1)
-    addDataData(node, -1.0)
-    addDataData(node, 1.0)
-    addDataData(node, 20.0)
-    addDataData(node, 1.0)
-    addDataData(node, 3.0)
-    addDataData(node, 5.0)
-    addDataData(node, 7.0)
-    addDataData(node, 9.0)
-    addDataData(node, 11.0)
-    addDataData(node, 13.0)
-    addDataData(node, 15.0)
-    addDataData(node, -1.0)
-    addDataData(node, -1.0)
-    addDataData(node, 600.0)
-    addDataData(node, 400.0)
-
-    node = addNode(outnode, 'Array', **{'as': 'objectsParameters'},
-        scilabClass='ScilabList')
-
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'nbZerosCrossing'}, height=1, width=1)
-    addDataData(node, 0.0)
-
-    node = addDataNode(outnode, 'ScilabDouble', **{'as': 'nmode'}, height=1, width=1)
-    addDataData(node, 0.0)
-
-    node = addNode(outnode, 'Array', **{'as': 'oDState'},
-        scilabClass='ScilabList')
-
-    node = addNode(outnode, 'Array', **{'as': 'equations'},
-        scilabClass='ScilabList')
-
-    node = addNode(outnode, 'mxGeometry', **{'as': 'geometry'},
-        height=geometry['height'], width=geometry['width'], x=geometry['x'], y=geometry['y'])
-
     return outnode
 
 def get_from_CSCOPE(cell):
