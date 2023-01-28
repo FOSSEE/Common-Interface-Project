ec731abed48923f0f2b328ce2b91a2dc9f6a87ac
Sunil Shetye <sunilshetye@rocketmail.com>
2023-01-28 20:33:45 +0530

    npm start

--- blocks/eda-frontend/src/redux/actions/authActions.js
+++ blocks/eda-frontend/src/redux/actions/authActions.js
@@ -64,7 +64,7 @@ export const login = (username, password, toUrl) => {
 
   return function (dispatch) {
     const allowedUrls = [
-      "/editor"
+      '/editor'
     ]
     api.post('auth/token/login/', body)
       .then((res) => {
