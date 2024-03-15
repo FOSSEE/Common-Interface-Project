446d82d7a734e697f842a28bc56c64e7379e0a51
Suchita Lad <suchitalad2738@gmail.com>
2024-03-15 16:46:06 +0530

    Updated CONST CONST_m CVS ENDBLK END_c GAINBLK GENSQR_f PROD_f SATURATION blocks files

--- blocks/Xcos/blocks/CVS.py
+++ blocks/Xcos/blocks/CVS.py
@@ -10,6 +10,51 @@ def CVS(outroot, attribid, ordering, geometry, parameters):
                          dependsOnT='1')
 
     addExprsNode(outnode, TYPE_DOUBLE, 0, parameters)
+    addTypeNode(outnode, TYPE_DOUBLE, AS_REAL_PARAM, 0,
+                [])
+    addTypeNode(outnode, TYPE_DOUBLE, AS_INT_PARAM, 0, [])
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_OBJ_PARAM, parameters)
+    array = ['0']
+    addPrecisionNode(outnode, TYPE_INTEGER, AS_NBZERO, 1, array)
+    addPrecisionNode(outnode, TYPE_INTEGER, AS_NMODE, 1, array)
+    addTypeNode(outnode, TYPE_DOUBLE, AS_STATE, 0, [])
+    addTypeNode(outnode, TYPE_DOUBLE, AS_DSTATE, 0, [])
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_ODSTATE, parameters)
+    equationsArrayNode = addArrayNode(outnode, scilabClass="ScilabTList",
+                                      **{'as': 'equations'})
+
+    # Add ScilabString nodes to equationsArrayNode
+    scilabStringParameters = ["modelica", "model",
+                              "inputs", "outputs",
+                              "parameters"]
+    addScilabStringNode(equationsArrayNode, width=5,
+                        parameters=scilabStringParameters)
+
+    # Add additional ScilabString nodes to equationsArrayNode
+    additionalStringNode = addDataNode(equationsArrayNode,
+                                       'ScilabString',
+                                       height=1, width=1)
+    addDataData(additionalStringNode, "CVS")
+
+    additionalScilabStrings = ["vin", "p"]
+    additionalStringNode = addDataNode(equationsArrayNode,
+                                       'ScilabString',
+                                       height=2, width=1)
+    for param in additionalScilabStrings:
+        addDataData(additionalStringNode, param)
+
+    additionalStringNode = addDataNode(equationsArrayNode,
+                                       'ScilabString',
+                                       height=1, width=1)
+    addDataData(additionalStringNode, "n")
+
+    innerArrayNode = addArrayNode(equationsArrayNode,
+                                  scilabClass="ScilabList")
+    addScilabDBNode(innerArrayNode, 0)
+    addScilabDBNode(innerArrayNode, 0)
+    addScilabDBNode(innerArrayNode, 0)
+    addgeometryNode(outnode, GEOMETRY, geometry['height'],
+                    geometry['width'], geometry['x'], geometry['y'])
 
     return outnode
 
--- blocks/Xcos/blocks/ENDBLK.py
+++ blocks/Xcos/blocks/ENDBLK.py
@@ -1,4 +1,9 @@
+from blocks.END_c import END_c
 from common.AAAAAA import *
+
+block_id = ['-76454188:14e1ef4db89:-7d58', '-76454188:14e1ef4db8a:-7d58',
+            '-76454188:14e1ef4db88:-7d55']
+
 
 def ENDBLK(outroot, attribid, ordering, geometry, parameters):
     func_name = 'ENDBLK'
@@ -9,6 +14,43 @@ def ENDBLK(outroot, attribid, ordering, geometry, parameters):
                          func_name, BLOCKTYPE_H)
 
     addExprsNode(outnode, TYPE_DOUBLE, 0, parameters)
+    addTypeNode(outnode, TYPE_DOUBLE, AS_REAL_PARAM, 0,
+                [])
+    addTypeNode(outnode, TYPE_DOUBLE, AS_INT_PARAM, 0, [])
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_OBJ_PARAM, parameters)
+    array = ['0']
+    addPrecisionNode(outnode, TYPE_INTEGER, AS_NBZERO, 1, array)
+    addPrecisionNode(outnode, TYPE_INTEGER, AS_NMODE, 1, array)
+    addTypeNode(outnode, TYPE_DOUBLE, AS_STATE, 0, [])
+    addTypeNode(outnode, TYPE_DOUBLE, AS_DSTATE, 0, [])
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_ODSTATE, parameters)
+    # Create the outer Array node for equations
+    addArrayNode(outnode, scilabClass="ScilabList",
+                                      **{'as': 'equations'})
+    addgeometryNode(outnode, GEOMETRY, geometry['height'],
+                    geometry['width'], geometry['x'], geometry['y'])
+
+    # Create the SuperBlockDiagram element
+    SuperBlockDiagram = addSuperNode(outnode, TYPE_SUPER,
+                                     a="child",
+                                     background="-1",
+                                     gridEnabled="1",
+                                     title="")
+    Array = addSuperBlkNode(SuperBlockDiagram, TYPE_ARRAY,
+                            a="context",
+                            scilabClass="String[]")
+    superAddNode(Array, TYPE_ADD, value="")
+
+    mxGraphModel = addmxGraphModelNode(SuperBlockDiagram,
+                                       TYPE_MODEL, a="model")
+    root = addNode(mxGraphModel, TYPE_ROOT)
+    addmxCellNode(root, TYPE_MXCELL,
+                  id=block_id[0])
+    addmxCellNode(root, TYPE_MXCELL,
+                  id=block_id[1],
+                  parent=block_id[0])
+
+    END_c(root, block_id[2], ordering, geometry, parameters)
 
     return outnode
 
