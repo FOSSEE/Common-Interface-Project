228bda06a5ae2ae79d563dcd6e7fc8a437cadf56
Suchita Lad <suchitalad2738@gmail.com>
2024-03-20 17:41:11 +0530

    Updated AFFICH CONVERT COSBLK_f FROM GOTO INTMUL PRODUCT SINBLK_f SQRT TrigFun blocks files

--- blocks/Xcos/blocks/AFFICH_m.py
+++ blocks/Xcos/blocks/AFFICH_m.py
@@ -10,6 +10,23 @@ def AFFICH_m(outroot, attribid, ordering, geometry, parameters):
                          dependsOnU='1')
 
     addExprsNode(outnode, TYPE_STRING, 7, parameters)
+    addSciDBNode(outnode, TYPE_DOUBLE, AS_REAL_PARAM,
+                 0, [])
+    array = ["1", "1", "1", "1000", "5", "1", "1"]
+    addPrecNode(outnode, TYPE_INTEGER, AS_INT_PARAM, 7, array)
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_OBJ_PARAM, parameters)
+    array = ['0']
+    addPrecisionNode(outnode, TYPE_INTEGER, AS_NBZERO, 1, array)
+    addPrecisionNode(outnode, TYPE_INTEGER, AS_NMODE, 1, array)
+    addTypeNode(outnode, TYPE_DOUBLE, AS_STATE, 0, [])
+    # addTypeNode(outnode, TYPE_DOUBLE, AS_DSTATE, 0, [])
+    addSciDBNode(outnode, TYPE_DOUBLE, AS_DSTATE,
+                 7, realParts=[-1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0])
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_ODSTATE, parameters)
+    addArrayNode(outnode, scilabClass="ScilabList",
+                                      **{'as': 'equations'})
+    addgeometryNode(outnode, GEOMETRY, geometry['height'],
+                    geometry['width'], geometry['x'], geometry['y'])
 
     return outnode
 
