3c903f7c0e61e05fad4f67425c87def6dc4f4b57
Suchita Lad <suchitalad2738@gmail.com>
2024-05-15 17:54:34 +0530

    Revert "Updated files of blocks"
    This reverts commit f350d325c59e0f51e7c2e4361a4d28185557a314.

--- blocks/Xcos/blocks/CLKOUT_f.py
+++ blocks/Xcos/blocks/CLKOUT_f.py
@@ -12,19 +12,19 @@ def CLKOUT_f(outroot, attribid, ordering, geometry, parameters, parent=1):
     input_list = parameters
     output_list = [str(int(float(num)) + 1) for num in input_list]
 
-    addExprsNode(outnode, TYPE_STRING, 1, output_list)
+    addExprsNode(outnode, TYPE_STRING, 1, parameters)
     addTypeNode(outnode, TYPE_DOUBLE, AS_REAL_PARAM, 0,
                 [])
     array = ['1']
     addPrecisionNode(outnode, TYPE_INTEGER, AS_INT_PARAM, 1, array)
-    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_OBJ_PARAM, output_list)
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_OBJ_PARAM, parameters)
     arr = ['0']
     addPrecisionNode(outnode, TYPE_INTEGER, AS_NBZERO, 1, arr)
     addPrecisionNode(outnode, TYPE_INTEGER, AS_NMODE, 1, arr)
     addTypeNode(outnode, TYPE_DOUBLE, AS_STATE, 0, [])
     addTypeNode(outnode, TYPE_DOUBLE, AS_DSTATE, 0, [])
-    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_ODSTATE, output_list)
-    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_EQUATIONS, output_list)
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_ODSTATE, parameters)
+    addObjNode(outnode, TYPE_ARRAY, CLASS_LIST, AS_EQUATIONS, parameters)
     addgeometryNode(outnode, GEOMETRY, geometry['height'],
                     geometry['width'], geometry['x'], geometry['y'])
 
