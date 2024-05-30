e9799049213c09273ea7d82709c958f9515f855b
Sunil Shetye <sunilshetye@rocketmail.com>
2024-05-30 12:12:29 +0530

    fix token name

--- blocks/authAPI/views.py
+++ blocks/authAPI/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponseNotFound
 from djoser import utils
 from djoser.serializers import TokenSerializer
 from authAPI.serializers import TokenCreateSerializer
+from blocks.settings import DOMAIN
 
 Token = djoser_settings.TOKEN_MODEL
 
@@ -36,45 +37,43 @@ def GoogleOAuth2(request):
     state = request.GET.get('state', None)
     code = request.GET.get('code', None)
 
-    if not (state is None) or not (code is None):
-        client_id = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY
-        client_secret = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
+    if state is None or state == '' or code is None or code == '':
+        return HttpResponseNotFound("<h1>Page Not Found</h1>")
 
-        google = OAuth2Session(
-            client_id,
-            redirect_uri=settings.GOOGLE_OAUTH_REDIRECT_URI,
-            state=state
-        )
-        google.fetch_token(
-            'https://accounts.google.com/o/oauth2/token',
-            client_secret=client_secret,
-            code=code
-        )
+    client_id = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY
+    client_secret = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
 
-        user_info = google.get(
-            'https://www.googleapis.com/oauth2/v1/userinfo').json()
+    google = OAuth2Session(
+        client_id,
+        redirect_uri=settings.GOOGLE_OAUTH_REDIRECT_URI,
+        state=state
+    )
+    google.fetch_token(
+        'https://accounts.google.com/o/oauth2/token',
+        client_secret=client_secret,
+        code=code
+    )
 
-        if user_info['email']:
-            user, created = get_user_model().objects.get_or_create(
-                email=user_info['email'])
-            if created:
-                # If User was created
-                # Set name to firstname_lastname1209
-                username = user_info['name'].strip().replace(
-                    ' ', '_') + str(randint(0, 9999))
-                user.username = username
-                user.save()
-            token, created = Token.objects.get_or_create(user=user)
+    user_info = google.get(
+        'https://www.googleapis.com/oauth2/v1/userinfo').json()
 
-            protocol = 'https://' if request.is_secure() else 'http://'
-            web_url = protocol + request.get_host() + '/eda/#/login'
+    if user_info['email']:
+        user, created = get_user_model().objects.get_or_create(
+            email=user_info['email'])
+        if created:
+            # If User was created, set name to email
+            user.username = user_info['email']
+            user.save()
+        token, created = Token.objects.get_or_create(user=user)
 
-            return render(request, 'google_callback.html',
-                          {
-                              "token": token,
-                              "url": web_url
-                          })
-    return HttpResponseNotFound("<h1>Page Not Found</h1>")
+        protocol = 'https://' if request.is_secure() else 'http://'
+        web_url = protocol + DOMAIN + '/#/dashboard'
+
+        return render(request, 'google_callback.html',
+                      {
+                          "token": token,
+                          "url": web_url
+                      })
 
 
 class CustomTokenCreateView(utils.ActionViewMixin, generics.GenericAPIView):
