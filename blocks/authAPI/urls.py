efba28d731c315dc2da7755622167818c6278717
Sunil Shetye <sunilshetye@rocketmail.com>
2024-07-08 16:08:02 +0530

    update Django and djangorestframework

--- blocks/authAPI/urls.py
+++ blocks/authAPI/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import re_path
 from authAPI import views as authAPI_views
 
 
 urlpatterns = [
     # GitHub OAuth2 callback
-    url(r'^github-callback', authAPI_views.GitHubOAuth2, name='github-callback'),
-    url(r'^google-callback', authAPI_views.GoogleOAuth2),
-    url(r'^users/activate/(?P<uid>[\w-]+)/(?P<token>[\w-]+)/$',
+    re_path(r'^github-callback', authAPI_views.GitHubOAuth2, name='github-callback'),
+    re_path(r'^google-callback', authAPI_views.GoogleOAuth2),
+    re_path(r'^users/activate/(?P<uid>[\w-]+)/(?P<token>[\w-]+)/$',
         authAPI_views.activate_user),
-    url(r'user/token/', authAPI_views.CustomTokenCreateView.as_view())
+    re_path(r'user/token/', authAPI_views.CustomTokenCreateView.as_view())
 ]
