ee4be99f2e66216e225fcbd714b8d0155feab971
Sunil Shetye <sunilshetye@rocketmail.com>
2024-09-03 11:27:36 +0530

    npm install react-redux@9.1.2 redux@5.0.1 redux-thunk@3.1.0

--- blocks/eda-frontend/src/redux/store.js
+++ blocks/eda-frontend/src/redux/store.js
@@ -1,8 +1,8 @@
 // Initialize Redux store which holds the whole state tree of application.
 import reducer from './reducers/index'
 import { createStore, applyMiddleware } from 'redux'
-import reduxThunk from 'redux-thunk'
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
+import { thunk } from 'redux-thunk'
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
 const store = createStoreWithMiddleware(reducer)
 
 export default store
