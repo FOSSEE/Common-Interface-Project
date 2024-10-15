45cdd9ba2c075eb808a1d22ebced4c45c4c8a635
Sunil Shetye <sunilshetye@rocketmail.com>
2024-10-15 12:58:20 +0530

    convert while loop to setTimeout calls for TailSpin to work better

--- blocks/eda-frontend/src/utils/GalleryUtils.js
+++ blocks/eda-frontend/src/utils/GalleryUtils.js
@@ -20,8 +20,9 @@ const getSplitXsltProcessor = async () => {
   return processor
 }
 
+const splitBlock = /<SplitBlock /g
+
 export const transformXcos = async (xmlDoc) => {
-  const splitBlock = /<SplitBlock /g
   const splitProcessor = await getSplitXsltProcessor()
   let dataDump = new XMLSerializer().serializeToString(xmlDoc)
   let count = dataDump.match(splitBlock).length
@@ -33,10 +34,65 @@ export const transformXcos = async (xmlDoc) => {
     const newCount = matches ? matches.length : 0
     if (newCount !== count - 1) {
       console.error('newCount=', newCount, ', count=', count)
+      throw new Error('count mismatch')
     }
     count = newCount
     console.log('count=', count)
   }
+  const processor = await getXsltProcessor()
+  xmlDoc = processor.transformToDocument(xmlDoc)
+  return xmlDoc
+}
+
+const removeOneSplit = (xmlDoc, count, splitProcessor) => {
+  if (count === 0) {
+    return { xmlDoc, count }
+  }
+
+  xmlDoc = splitProcessor.transformToDocument(xmlDoc)
+  const dataDump = new XMLSerializer().serializeToString(xmlDoc)
+  const matches = dataDump.match(splitBlock)
+  const newCount = matches ? matches.length : 0
+  if (newCount !== count - 1) {
+    console.error('newCount=', newCount, ', count=', count)
+    throw new Error('count mismatch')
+  }
+  return { xmlDoc, count: newCount }
+}
+
+const removeSplits = async (xmlDoc, splitProcessor, delay) => {
+  const removeNextSplit = async (xmlDoc, count, splitProcessor) => {
+    const rv = removeOneSplit(xmlDoc, count, splitProcessor)
+    xmlDoc = rv.xmlDoc
+    count = rv.count
+    console.log('count=', count)
+    if (count === 0) {
+      return xmlDoc
+    }
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const result = removeNextSplit(xmlDoc, count, splitProcessor)
+        resolve(result)
+      }, delay)
+    })
+  }
+
+  const dataDump = new XMLSerializer().serializeToString(xmlDoc)
+  const count = dataDump.match(splitBlock).length
+  console.log('count=', count)
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = removeNextSplit(xmlDoc, count, splitProcessor)
+      resolve(result)
+    }, delay)
+  })
+}
+
+export const transformXcos2 = async (xmlDoc) => {
+  const splitProcessor = await getSplitXsltProcessor()
+  xmlDoc = await removeSplits(xmlDoc, splitProcessor, 1)
   const processor = await getXsltProcessor()
   xmlDoc = processor.transformToDocument(xmlDoc)
   return xmlDoc
