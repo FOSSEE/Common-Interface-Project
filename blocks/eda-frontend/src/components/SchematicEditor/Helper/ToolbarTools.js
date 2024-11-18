d6e66dcbfe85bb28a8b2d76c3138da7e0e6c0932
Suchita Lad <suchitalad2738@gmail.com>
2024-11-18 17:47:49 +0530

    Updated firstportrotation code

--- blocks/eda-frontend/src/components/SchematicEditor/Helper/ToolbarTools.js
+++ blocks/eda-frontend/src/components/SchematicEditor/Helper/ToolbarTools.js
@@ -492,6 +492,7 @@ function parseXmlToGraph (xmlDoc, graph) {
   const parent = graph.getDefaultParent()
   let v1
   let blockrotation
+  let firstportrotation
   graph.getModel().beginUpdate()
 
   let oldcellslength = 0
@@ -517,11 +518,13 @@ function parseXmlToGraph (xmlDoc, graph) {
           }
           const style = cellAttrs.style.value
           const styleObject = styleToObject(style)
+          console.log('blckname:', styleObject)
           if (styleObject.rotation === undefined) {
             blockrotation = 0
           } else {
             blockrotation = parseInt(styleObject.rotation)
           }
+          firstportrotation = null
           const vertexId = cellAttrs.id.value
           const geom = cellChildren[0].attributes
           const xPos = (geom.x !== undefined) ? Number(geom.x.value) : 0
@@ -582,6 +585,7 @@ function parseXmlToGraph (xmlDoc, graph) {
           } else {
             portrotation = parseInt(styleObject.rotation)
           }
+
           let rotation = portrotation - blockrotation
           if (stylename === 'ControlPort' || stylename === 'CommandPort') {
             rotation -= 90
@@ -589,6 +593,13 @@ function parseXmlToGraph (xmlDoc, graph) {
           if (rotation < 0) {
             rotation += 360
           }
+          if (firstportrotation === null) {
+            firstportrotation = rotation
+          } else if (rotation !== firstportrotation) {
+            console.log('firstportrotation is not equal to rotation', firstportrotation, rotation)
+            rotation = firstportrotation
+          }
+          console.log('rotation:', rotation)
 
           const vertexId = cellAttrs.id.value
           const geom = cellChildren[0].attributes
@@ -668,18 +679,18 @@ function parseXmlToGraph (xmlDoc, graph) {
           const sourceType = getPortType(sourceCell)
           const targetType = getPortType(targetCell)
           if (sourceType.type2 !== OutputPort && targetType.type2 !== InputPort) {
-            console.log('switch', source, target)
-            const tmp = source
-            source = target
-            target = tmp
+            console.log('switch', source, target, points)
+            // const tmp = source
+            // source = target
+            // target = tmp
             console.log('sourceCell:', sourceCell)
-            const tmpCell = sourceCell
-            sourceCell = targetCell
-            targetCell = tmpCell
+            // const tmpCell = sourceCell
+            // sourceCell = targetCell
+            // targetCell = tmpCell
             // const tmpsourcePoint = sourcePoint
             // sourcePoint = targetPoint
             // targetPoint = tmpsourcePoint
-            points.reverse()
+            // points.reverse()
           }
 
           try {
