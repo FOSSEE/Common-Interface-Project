bca91a77153e1b2e844015c64e97eb77046ce72b
Sunil Shetye <sunilshetye@rocketmail.com>
2025-04-15 12:36:08 +0530

    update node to 20.19.0
    update eslint to 9.24.0

---
+++ blocks/eda-frontend/eslint.config.js
@@ -0,0 +1,70 @@
+// eslint.config.js
+import js from '@eslint/js'
+import babelParser from '@babel/eslint-parser'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import importPlugin from 'eslint-plugin-import'
+import nPlugin from 'eslint-plugin-n'
+import promisePlugin from 'eslint-plugin-promise'
+
+export default [
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react']
+        },
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+      },
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      }
+    },
+
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      import: importPlugin,
+      n: nPlugin,
+      promise: promisePlugin
+    },
+
+    settings: {
+      node: {
+        version: '>=18.0.0'
+      },
+      react: {
+        version: 'detect'
+      }
+    },
+
+    rules: {
+      // Style rules
+      'space-before-function-paren': ['error', 'always'],
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'comma-dangle': ['error', 'never'],
+      indent: ['error', 2],
+      'no-var': 'error',
+      'prefer-const': 'error',
+
+      // React/React Hooks
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  }
+]
