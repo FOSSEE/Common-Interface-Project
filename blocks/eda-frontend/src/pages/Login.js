965053cd01f78d021d1d0971fb1c3c9b79c73b31
Suchita Lad <suchitalad2738@gmail.com>
2025-07-11 23:48:02 +0530

    Updated template of reset password

--- blocks/eda-frontend/src/pages/Login.js
+++ blocks/eda-frontend/src/pages/Login.js
@@ -189,7 +189,7 @@ export default function SignIn (props) {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link component={RouterLink} to='#' variant='body2'>
+              <Link component={RouterLink} to='/forgotpwd' variant='body2'>
                 Forgot password?
               </Link>
             </Grid>
