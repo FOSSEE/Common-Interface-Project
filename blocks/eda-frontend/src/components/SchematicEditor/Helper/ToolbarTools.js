1d86d0778810bafe28349885ba224f8a7d433f42
Suchita Lad <suchitalad2738@gmail.com>
2025-07-21 13:18:18 +0530

    updated port position logic of blocks

--- blocks/eda-frontend/src/components/SchematicEditor/Helper/ToolbarTools.js
+++ blocks/eda-frontend/src/components/SchematicEditor/Helper/ToolbarTools.js
@@ -673,6 +673,19 @@ function parseXmlToGraph (xmlDoc, graph) {
       remainingcells = []
       console.log('cellslength=', cellslength, ', oldcellslength=', oldcellslength)
     }
+    const allCells = model.getChildren(graph.getDefaultParent())
+    allCells.forEach(cell => {
+      if (cell.CellType === 'Component') {
+        const styleMap = styleToObject(model.getStyle(cell) || '')
+        const flip = styleMap.flip === 'true'
+        const mirror = styleMap.mirror === 'true'
+        const rotation = parseInt(styleMap.rotation || '0')
+
+        if (flip || mirror) {
+          flipMirrorPorts(cell, flip, mirror, model, false)
+        }
+      }
+    })
     graph.view.refresh()
   } finally {
     model.endUpdate()
