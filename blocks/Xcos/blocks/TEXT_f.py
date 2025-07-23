9be970db47d744a26e2c4c413bc9f9b2dff80d37
Suchita Lad <suchitalad2738@gmail.com>
2025-07-23 16:57:36 +0530

    Updated TEXT_f file

--- blocks/Xcos/blocks/TEXT_f.py
+++ blocks/Xcos/blocks/TEXT_f.py
@@ -19,12 +19,12 @@ def TEXT_f(outroot, attribid, ordering, geometry, parameters, parent=1, style=No
 
 
 def get_from_TEXT_f(cell):
-    parameters = cell.get('parameters', [])
-    display_parameter = parameters[0] if parameters else ""
+    (parameters, display_parameter, eiv, iiv, con, eov, iov, com) = getParametersFromExprsNode(None)
 
-    # Assuming TEXT_f has no input/output ports
-    eiv, iiv, con, eov, iov, com = 0, "", 0, "", "", ""
+    parameters = cell.get('parameters', [])
+
+    display_parameter = parameters[0] if parameters else ""
 
     ports = [eiv, iiv, con, eov, iov, com]
 
-    return parameters, display_parameter, ports
+    return (parameters, display_parameter, ports)
