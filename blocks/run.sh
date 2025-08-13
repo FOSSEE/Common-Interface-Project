c5e1e1e46c340827eef8d8cc85f4ab57972d21fd
Sunil Shetye <sunilshetye@rocketmail.com>
2025-08-13 07:45:25 +0530

    copy the collectstatic resources

--- blocks/run.sh
+++ blocks/run.sh
@@ -18,6 +18,4 @@ fi
 cd ..
 python manage.py migrate
 python manage.py loaddata saveAPI xcosblocks
-python manage.py collectstatic --no-input
-rm -rf /var/www/html/static/{admin,rest_framework}
 python manage.py runserver
