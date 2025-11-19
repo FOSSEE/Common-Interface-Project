5a5d038f2b60bcd7ec4771f7093748e2f368c903
Sunil Shetye <sunilshetye@rocketmail.com>
2025-11-19 16:39:15 +0530

    remove unused ResistorShape code

--- blocks/eda-frontend/src/components/SchematicEditor/Helper/ComponentDrag.js
+++ blocks/eda-frontend/src/components/SchematicEditor/Helper/ComponentDrag.js
@@ -34,8 +34,6 @@ const {
   mxConnectionHandler,
   mxConstants,
   mxGraphHandler,
-  mxCylinder,
-  mxCellRenderer,
   mxConstraintHandler,
   mxImage,
   mxCodec
@@ -765,31 +763,6 @@ export default function LoadGrid (container, sidebar, outline, setMainDiagramBac
 
     return style
   }
-
-  function ResistorShape () { }
-  ResistorShape.prototype = new mxCylinder()
-  ResistorShape.prototype.constructor = ResistorShape
-
-  ResistorShape.prototype.redrawPath = function (path, x, y, w, h, isForeground) {
-    const dx = w / 16
-
-    if (isForeground) {
-      path.moveTo(0, h / 2)
-      path.lineTo(2 * dx, h / 2)
-      path.lineTo(3 * dx, 0)
-      path.lineTo(5 * dx, h)
-      path.lineTo(7 * dx, 0)
-      path.lineTo(9 * dx, h)
-      path.lineTo(11 * dx, 0)
-      path.lineTo(13 * dx, h)
-      path.lineTo(14 * dx, h / 2)
-      path.lineTo(16 * dx, h / 2)
-
-      path.end()
-    }
-  }
-
-  mxCellRenderer.registerShape('resistor', ResistorShape)
 
   mxEdgeStyle.WireConnector = function (state, source, target, hints, result) {
     // Creates array of all way- and terminalpoints
